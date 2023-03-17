2423cb6bcb06cd95060950f491e5f68fb6573b8a
mizuirorivi <onodaha@gmail.com>
2023-03-17 21:38:13 +0900

    Update 'direction' to 'float' in toggleterm.lua

--- lua/plugins/config/toggleterm.lua
+++ lua/plugins/config/toggleterm.lua
@@ -10,7 +10,7 @@ require("toggleterm").setup{
   shading_factor = 1, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
   start_in_insert = true,
   persist_size = true,
-  direction = 'tab'
+  direction = 'float'
 }
 
 -- require("toggleterm").setup{
