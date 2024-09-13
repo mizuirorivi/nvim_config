ac6e31ed4448f1c6188b1315dfa1db14eb27b486
mizuirorivi <onodaha@gmail.com>
2024-09-14 01:27:39 +0900

    Add new plugin configuration for leetcode.nvimin lazy.lua config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -306,5 +306,24 @@ require("lazy").setup({
       })
     end,
   },
-})
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
 
+      -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "rcarriga/nvim-notify",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      -- configuration goes here
+      plugins = {
+        non_standalone = true,
+      }
+    },
+  },
+})
